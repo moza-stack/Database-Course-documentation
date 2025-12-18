597c3bb2d4c0d543e44cc3d2c18b5b9bb37ae250
“karim <moza.m@gmail.com>
2025-12-18 12:23:46 +0400

    adding SQL FILE

--- Database Search/SQL Company/SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-create database companyDB
-use companyDB
-
-create table employee(Employeeid int primary key,
-Firstname varchar(50) not null,
-Lastname varchar(50) not null,
-Department varchar(50),
-Salary Decimal(10,2)check(salary>0),)
