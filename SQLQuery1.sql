e5f5f894b6bb71c5c2ac9b57293d0b8e420bce8a
“karim <moza.m@gmail.com>
2025-12-31 08:17:11 +0400

    adding files

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+select* from Library;
+select* from Members;
+select * from Book;
+select * from Staff ;
+select * from Loan;
+select* from Payment ;
+select* from  Review;
+
+---1. Library Book Inventory Report----
+
+
+
+
